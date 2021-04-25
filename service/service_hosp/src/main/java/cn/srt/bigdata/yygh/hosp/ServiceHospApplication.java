6586090957b2f6de04fed59f611aff8ffa5cee74
drestart <1114815474@qq.com>
2021-04-25 23:28:20 +0800

    04.25 commit

---
+++ service/service_hosp/src/main/java/cn/srt/bigdata/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,20 @@
+package cn.srt.bigdata.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+/**
+ * @author shkstart
+ * @create 2021-04-25 23:20
+ */
+
+@SpringBootApplication
+public class ServiceHospApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
