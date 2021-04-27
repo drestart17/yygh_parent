5aa1859ba3a55f637eccf571d6f081edfbe5776f
drestart17 <drestart17@163.com>
2021-04-27 23:21:42 +0800

    04.27 commit

--- service/service_hosp/src/main/java/cn/srt/bigdata/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/cn/srt/bigdata/yygh/hosp/controller/HospitalSetController.java
@@ -1,8 +1,12 @@
 package cn.srt.bigdata.yygh.hosp.controller;
 
+import cn.srt.bigdata.common.result.Result;
 import cn.srt.bigdata.yygh.hosp.service.HospitalSetService;
 import cn.srt.bigdata.yygh.model.hosp.HospitalSet;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,26 +18,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
+@Api(tags = "医院设置接口")
 public class HospitalSetController {
 
     //service注入
-
+    //http:localhost:8201/admin/hosp/hospitalSet/findAll
     @Autowired
     private HospitalSetService hospitalSetService;
 
     @GetMapping("findAll")
-    public HospitalSet findAll() {
+    @ApiOperation(value = "查询所有数据")
+    public Result findAll() {
 
         //查询所有数据
         HospitalSet list = hospitalSetService.getById(2);
-        return list;
+        return Result.ok(list);
     }
 
     @DeleteMapping("{id}")
-    public boolean deleteById(@PathVariable Long id) {
+    @ApiOperation(value = "逻辑删除医院设置")
+    public Result deleteById(@ApiParam(name = "idhhh",value = "删除的ID",required = true) @PathVariable Long id) {
 
         //根据ID删除记录
         boolean flag = hospitalSetService.removeById(id);
-        return flag;
+        if(flag = true) {
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
     }
 }
