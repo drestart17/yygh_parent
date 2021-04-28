ba72f691db02d6b39393724a8cb07cd483a7b75f
drestart17 <drestart17@163.com>
2021-04-28 23:10:28 +0800

    04.28 commit

--- service/service_hosp/src/main/java/cn/srt/bigdata/yygh/hosp/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/cn/srt/bigdata/yygh/hosp/controller/HospitalSetController.java
@@ -1,6 +1,7 @@
 package cn.srt.bigdata.yygh.hosp.controller;
 
 import cn.srt.bigdata.common.result.Result;
+import cn.srt.bigdata.common.utils.MD5;
 import cn.srt.bigdata.yygh.hosp.service.HospitalSetService;
 import cn.srt.bigdata.yygh.model.hosp.HospitalSet;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Random;
 
 /**
  * @create 2021-04-26 23:30
@@ -46,5 +48,112 @@ public class HospitalSetController {
         }else {
             return Result.fail();
         }
+    }
+
+    //3.条件查询带分页@ApiOperation(value = "根据医院名称和ID分页查询医院设置")@PostMapping("findPageHospSet/{current}/{limit}")public Result findPageHospSet(@PathVariable Long current,                              @PathVariable Long limit,                              @RequestBody(required = false) HospitalSetQueryVo hospitalSetQueryVo) {    //3.1创建条件查询对象    QueryWrapper<HospitalSet> hospitalSetWrapper = new QueryWrapper<>();    //3.2如果非required条件需要判断条件是否为空    String hosname = hospitalSetQueryVo.getHosname();    String hoscode = hospitalSetQueryVo.getHoscode();    if(!StringUtils.isEmpty(hosname)) {        hospitalSetWrapper.like("hosname",hospitalSetQueryVo.getHosname());    }    if(!StringUtils.isEmpty(hoscode)) {        hospitalSetWrapper.eq("hoscode", hospitalSetQueryVo.getHoscode());    }    //创建分页对象    Page<HospitalSet> hospitalSetPage = new Page<>(current,limit);    Page<HospitalSet> resultPage = hospitalSetService.page(hospitalSetPage, hospitalSetWrapper);    return Result.ok(resultPage);}
+
+
+//4.添加医院设置
+    @PostMapping("saveHospitalSet")
+    public Result saveHospitalSet(@RequestBody HospitalSet hospitalSet) {
+
+
+        //1.设置默认状态 1：使用 0：不能使用
+        hospitalSet.setStatus(1);
+
+
+        //2.设置签名密钥：随机数 + 当前时间 + MD5加密
+        //2.1 生成一个随机数
+        Random random = new Random();
+        int randInt = random.nextInt(1000);
+        //2.2 获取系统当前时间
+        long curTimeStamp = System.currentTimeMillis();
+        //2.3 MD5加密
+        String encrypt = MD5.encrypt(curTimeStamp + "" + randInt);
+        //2.4 给signKey赋值
+        hospitalSet.setSignKey(encrypt);
+
+
+        //调用service层
+        boolean saveFlag = hospitalSetService.save(hospitalSet);
+
+
+        if(saveFlag) {
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
+
+    }
+
+
+//修改：5.1 根据ID查询医院设置
+
+
+    @GetMapping("getHospSet/{id}")
+    public Result getHospSet(@PathVariable Long id) {
+
+        //模拟异常
+        int a = 1/0;
+
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+
+
+        return Result.ok(hospitalSet);
+    }
+
+
+    //5.2 修改医院设置
+    @PostMapping("updateHospitalSet")
+    public Result updateHospitalSet(@RequestBody HospitalSet hospitalSet) {
+
+
+        boolean flag = hospitalSetService.updateById(hospitalSet);
+
+
+        if(flag) {
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
+    }
+
+
+    //6.批量删除医院设置接口
+    @DeleteMapping("batchRemove")
+    public Result batchRemove(@RequestBody List<Long> ids ) {
+
+
+        boolean flag = hospitalSetService.removeByIds(ids);
+        return Result.ok();
+    }
+
+    //7.医院设置锁定和解锁
+    @PutMapping("localHospitalSet/{id}/{status}")
+    public Result localHospitalSet(@PathVariable Long id,
+                                   @PathVariable Integer status) {
+
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+
+        hospitalSet.setStatus(status);
+        boolean flag = hospitalSetService.updateById(hospitalSet);
+        if(flag) {
+            return Result.ok();
+        }else {
+            return Result.fail();
+        }
+    }
+
+    //8.发送签名密钥
+    @PutMapping("sendKey/{id}")
+    public Result sendKey(@PathVariable Long id) {
+
+        HospitalSet hospitalSet = hospitalSetService.getById(id);
+
+        String signKey = hospitalSet.getSignKey();
+        String hoscode = hospitalSet.getHoscode();
+
+        //TODO 发送短信
+        return Result.ok();
     }
 }
